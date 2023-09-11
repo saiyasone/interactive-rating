d5906ce852c316e97b11e02ad43316683279928e
saiyasone <saiyasone595@gmail.com>
2023-09-11 10:41:37 +0700

    deploy all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'interactive-rating';
+  isDone = false;
+  isActive = '';
+
+  buttonList: any[] = ['1', '2', '3', '4', '5'];
+
+  setDone() {
+    this.isDone = !this.isDone;
+  }
+
+  setActive(active: string) {
+    if (active === this.isActive) {
+      return;
+    }
+
+    this.isActive = active;
+  }
+
+  onSave() {
+    if (!this.isActive) {
+      return;
+    }
+
+    this.setDone();
+  }
 }
